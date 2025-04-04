926a00417a6ba74016527a3fc81abde53bec5d74
Mehdi Ghaderi <mehdiii.ghaderi@gmail.com>
2025-04-04 12:50:13 +0200

    chore: add sample new customers

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer (customer_id, name, surname) VALUES
+                                                      (1, 'John', 'Smith'),
+                                                      (2, 'Emma', 'Johnson'),
+                                                      (3, 'Michael', 'Williams'),
+                                                      (4, 'Sophia', 'Brown'),
+                                                      (5, 'Robert', 'Davis');
